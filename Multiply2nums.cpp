41b325f52b8823c31e2d287598d07b0a5b79352d
Muhammad Zohaib Arshad <muhammadzohaibarshad18@gmail.com>
2021-10-28 02:15:55 +0500

    Seperate insertion of values

---
+++ Multiply2nums.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  double num1, num2, product;
+  cout << "Enter First value : "<<endl;
+  cin >> num1;
+  cout << "Enter Second value : "<<endl;
+  cin >> num2;
+  product = num1 * num2;
+  cout << "Product of two numbers are = " << product  <<endl;
+  return 0;
+}
